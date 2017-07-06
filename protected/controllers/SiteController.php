247aea78015d501d5e8459cc960ae98e4fbb3496
Chathuranga <chathuranga@minthrm.com>
2017-07-06 23:21:29 +0530

    Test

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -95,5 +95,10 @@ class SiteController extends Controller {
         Yii::app()->user->logout();
         $this->redirect(Yii::app()->homeUrl);
     }    
+    
+    public function actionTest() {  
+        echo 'test';
+    }  
+    
 
 }
