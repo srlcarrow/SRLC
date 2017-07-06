5dec4e81894669c07615fe115018331d8559aa1b
srlcarrow <srlcarrow@gmail.com>
2017-07-07 00:02:16 +0530

    jj

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -48,7 +48,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by SRLC.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
