6582d9a470bff2eeaf00ce43025cd779ba37d053
srlcflash <srlcflash@gmail.com>
2017-07-10 22:58:41 +0530

    Moified menu

--- protected/views/layouts/menu.php
+++ protected/views/layouts/menu.php
@@ -1,15 +1,38 @@
 <div class="navbar-fixed">
-    <nav class=" light-blue accent-3">
+    <nav class="light-blue accent-3">
         <div class="container">
             <div class="nav-wrapper">
                 <a class="brand-logo logo-link" href="#">
                     <img src="<?php echo Yii::app()->baseUrl . '/images/system/logo/logo.png'; ?>" alt="">
                 </a>
-                <ul class="hide-on-med-and-down right">
+                <ul class="hide-on-med-and-down right menu">
                     <li><a href="#">Admin</a></li>
                     <li><a href="#">Category</a></li>
+                    <li>
+                        <a href="#">Category </a>
+                        <ul>
+                            <li><a href="#">Sub 1</a></li>
+                            <li>
+                                <a href="#">Sub 2</a>
+                                <ul>
+                                    <li><a href="#">Sub 1</a></li>
+                                    <li>
+                                        <a href="#">Sub 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+
+
+                    </li>
                 </ul>
             </div>
         </div>
     </nav>
 </div>
+
+<script>
+    $(function () {
+        $(".dropdown-button").dropdown();
+    })
+</script>
