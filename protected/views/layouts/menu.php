d2af238ade430be196121ba4de6e1fb3578f4aa5
sanjeewa <sanjeewa@infoseek.lk>
2017-07-11 04:59:19 +0530

    navigation fixed

--- protected/views/layouts/menu.php
+++ protected/views/layouts/menu.php
@@ -1,25 +1,14 @@
 <div class="navbar-fixed">
-    
-    
-    <ul id="dropdown1" class="dropdown-content">
-        <li><a href="#!">one</a></li>
-        <li><a href="#!">two</a></li>
-        <!--<li class="divider"></li>-->
-        <li><a href="#!">three</a></li>
-    </ul>
-    
-    
     <nav class="light-blue accent-3">
         <div class="container">
             <div class="nav-wrapper">
                 <a class="brand-logo logo-link" href="#">
                     <img src="<?php echo Yii::app()->baseUrl . '/images/system/logo/logo.png'; ?>" alt="">
                 </a>
-                <ul class="hide-on-med-and-down right">
+                <ul class="hide-on-med-and-down right menu">
                     <li><a href="#">Admin</a></li>
                     <li><a href="#">Category</a></li>
-                    <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
-<!--                    <li>
+                    <li>
                         <a href="#">Category </a>
                         <ul>
                             <li><a href="#">Sub 1</a></li>
@@ -29,19 +18,11 @@
                                     <li><a href="#">Sub 1</a></li>
                                     <li>
                                         <a href="#">Sub 2</a>
-                                        <ul>
-                                            <li><a href="#">Sub 1</a></li>
-                                            <li>
-                                                <a href="#">Sub 2</a>
-                                            </li>
-                                        </ul>
                                     </li>
                                 </ul>
                             </li>
                         </ul>
-
-
-                    </li>-->
+                    </li>
                 </ul>
                 <?php
                 $menu = Links::model()->findAllByAttributes(array('lnk_parent_id'=>0),array('order'=>'lnk_order ASC'));              
@@ -57,3 +38,5 @@
         $(".dropdown-button").dropdown();
     })
 </script>
+
+
