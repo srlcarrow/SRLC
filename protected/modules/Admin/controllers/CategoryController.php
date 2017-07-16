93a4033b07997f62c8bc328ebff2d20c9fa8f634
srlcarrow <srlcarrow@gmail.com>
2017-07-16 10:08:06 +0530

    2017_07_16

--- protected/modules/Admin/controllers/CategoryController.php
+++ protected/modules/Admin/controllers/CategoryController.php
@@ -7,7 +7,33 @@ class CategoryController extends Controller {
     }
 
     public function actionViewCategoryData() {
-        $this->renderPartial('ajaxLoad/viewCategoryData');
+        $categories = AdmCategory::model()->findAll();
+        $this->renderPartial('ajaxLoad/viewCategoryData', array('categories' => $categories));
+    }
+
+    public function actionSaveCategory() {
+        try {
+            $model = new AdmCategory();
+            $model->cat_name = $_POST['name'];
+            $model->cat_order = $_POST['order'];
+            if ($model->save(false)) {
+                $this->msgHandler(200, "Successfully Saved...");
+            }
+        } catch (Exception $exc) {
+            $this->msgHandler(400, $exc->getTraceAsString());
+        }
+    }
+
+    public function actionDeleteCategory() {
+        try {
+            $id = $_POST['id'];
+            $model = new AdmCategory();
+            if ($model->deleteByPk($id)) {
+                $this->msgHandler(200, "Deleted Successfully...");
+            }
+        } catch (Exception $exc) {
+            $this->msgHandler(400, $exc->getTraceAsString());
+        }
     }
 
 }
--- protected/modules/Admin/views/Category/viewCategory.php
+++ protected/modules/Admin/views/Category/viewCategory.php
@@ -1,5 +1,6 @@
 <!--JS | Jquery Lib-->
 <script src="<?php echo Yii::app()->baseUrl . '/js/lib/jquery-3.2.1.min.js'; ?>"></script>
+<?php $form = $this->beginWidget('CActiveForm', array('id' => 'formAddCategory')); ?>
 <div class="row">
     <div class="col s12">
         <div class="card ">
@@ -9,13 +10,13 @@
                 <div class="row">
                     <div class="col s12 m8">
                         <div class="input-field">
-                            <input type="text">
+                            <input name="name" type="text">
                             <label >Category Name</label>
                         </div>
                     </div>
                     <div class="col s12 m4">
                         <div class="input-field">
-                            <input type="text">
+                            <input name="order" type="text">
                             <label>Category Order</label>
                         </div>
                     </div>
@@ -24,12 +25,12 @@
             </div>
             <div class="card-action right-align">
                 <button type="button" class=" btn waves-effect waves-light red lighten-1">Clear</button>
-                <button type="submit" class="btn waves-effect waves-light blue lighten-1">Save</button>
+                <button id="saveCategory" type="submit" class="btn waves-effect waves-light blue lighten-1">Save</button>
             </div>
         </div>
     </div>
 </div>
-
+<?php $this->endWidget(); ?>
 
 <!--Data Showing area-->
 <div class="ajaxLoad"></div>
@@ -47,10 +48,24 @@
             type: 'POST',
             url: "<?php echo Yii::app()->baseUrl . '/Admin/Category/ViewCategoryData'; ?>",
             data: '',
-            dataType: 'json',
             success: function (responce) {
-                
+                $(".ajaxLoad").html(responce);
             }
         });
     }
+    $('#saveCategory').click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: "<?php echo Yii::app()->baseUrl . '/Admin/Category/SaveCategory'; ?>",
+            data: $('#formAddCategory').serialize(),
+            dataType: 'json',
+            success: function (responce) {
+                if (responce.code == 200) {
+                    loadCategoryData();
+                }
+            }
+        });
+    });
+
 </script>
