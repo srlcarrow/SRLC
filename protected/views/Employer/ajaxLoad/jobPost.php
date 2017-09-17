9c5a1636431ce6efbc54f4afd9f7b1e773c76f66
Lanka <lanka@minthrm.com>
2017-09-17 18:30:41 +0530

    lanka 3

--- protected/views/Employer/ajaxLoad/jobPost.php
+++ protected/views/Employer/ajaxLoad/jobPost.php
@@ -1,31 +1,31 @@
 
 <div class="col-md-12">
     <div class="col-md-12">
-        <button class="cm-btn large right light-blue-4">Go to Job Post</button>
+        <button class="cm-btn large right light-blue-4">Post a Job</button>
     </div>
 
     <div class="col-md-12">
 
-        <table>
-            <colgroup>
-                <col>
-                <col>
-                <col>
-            </colgroup>
-
-            <thead>
-                <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </thead>
-
-            <tbody>
-            <tr>
-                <td></td>
-            </tr>
-            </tbody>
-        </table>
+<!--        <table>-->
+<!--            <colgroup>-->
+<!--                <col>-->
+<!--                <col>-->
+<!--                <col>-->
+<!--            </colgroup>-->
+<!---->
+<!--            <thead>-->
+<!--                <tr>-->
+<!--                    <td></td>-->
+<!--                    <td></td>-->
+<!--                </tr>-->
+<!--            </thead>-->
+<!---->
+<!--            <tbody>-->
+<!--            <tr>-->
+<!--                <td></td>-->
+<!--            </tr>-->
+<!--            </tbody>-->
+<!--        </table>-->
 
     </div>
 </div>
