e8764dc0b5812fb46b07c63f516cced092ae671e
Rajith S. Kathriarachchi <rajith@minthrm.com>
2017-09-17 19:34:39 +0530

    aaa2

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -6,7 +6,19 @@ class UserController extends Controller {
     }
 
     public function actionProfile() {
-        $this->render('profile');
+        $userId = Yii::app()->user->id;
+        $user = User::model()->findByAttributes(array('user_id' => $userId));
+        $userType = $user->user_type; 
+        
+        
+        if($userType==1){
+            $model = JsBasic::model()->findByAttributes(array('ref_jsbt_id' => $user->ref_emp_or_js_id));
+            $employment = JsEmploymentData::model()->findByAttributes(array('ref_js_id' => $user->ref_emp_or_js_id));
+        }else{
+            $model = new JsBasic();  
+            $employment = new JsEmploymentData();  
+        }       
+        $this->render('profile',array('model' => $model));  
     }
 
     public function actionPersonalInfo() {
