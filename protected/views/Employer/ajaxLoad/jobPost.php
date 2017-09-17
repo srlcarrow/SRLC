8478c2bf7d0dacd3e01edde4cced8f302a8a1153
Lanka <lanka@minthrm.com>
2017-09-17 15:50:38 +0530

    Merge branch 'master' of https://github.com/srlcarrow/SRLC
    # Conflicts:
    #	js/custom/common.js

--- protected/views/Employer/ajaxLoad/jobPost.php
+++ protected/views/Employer/ajaxLoad/jobPost.php
@@ -3,4 +3,30 @@
     <div class="col-md-12">
         <button class="cm-btn large right light-blue-4">Go to Job Post</button>
     </div>
+
+    <div class="col-md-12">
+
+        <table>
+            <colgroup>
+                <col>
+                <col>
+                <col>
+            </colgroup>
+
+            <thead>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
 </div>
+
