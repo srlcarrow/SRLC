4d3d327c4a881c9b58204cda31012682ef7c64d0
Lanka <lanka@minthrm.com>
2017-09-24 17:22:51 +0530

    Lanka 2

---
+++ protected/views/site/employer.php
@@ -0,0 +1,155 @@
+<div class="nav-bar-space"></div>
+<div class="container">
+    <div class="row mb-50">
+
+        <div class="col-md-10 col-md-offset-1 mt-60">
+
+            <div class="row">
+
+                <div class="col-md-4">
+                    <h2 class="f-30 text-black">Get immediate access to top talent</h2>
+
+                    <p class="mt-30 text-light-2 text-black">Lorem Ipsum is simply dummy text of the printing
+                        and typesetting industry. Lorem Ipsum has been
+                        the industry's standard</p>
+                </div>
+
+                <div class="col-md-8">
+                    <img src="<?php echo Yii::app()->baseUrl ?>/images/system/other/candidate/add.png" alt="">
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-md-12 pt-60 pb-60"></div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-5">
+                    <img src="<?php echo Yii::app()->baseUrl ?>/images/system/other/candidate/profile.png" alt="">
+                </div>
+
+                <div class="col-md-7">
+                    <div class="row">
+                        <h2 class="col-md-8 f-30 text-black">Pay as you wish</h2>
+                        <div class="col-md-11">
+                            <p class="mt-30 text-light-2 text-black">Lorem Ipsum is simply dummy text of the
+                                printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard</p>
+
+                            <p class="mt-30 text-light-2 text-black">Lorem Ipsum is simply dummy text of the
+                                printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-md-12 pt-60 pb-60"></div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-8">
+
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h2 class="f-30 text-black">Right jobs come to you</h2>
+
+                            <p class=" mt-30 text-light-2 text-black">Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry.
+                                Lorem Ipsum has been the industry's standard Lorem Ipsum is simply
+                                dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                the industry's standard</p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <img src="<?php echo Yii::app()->baseUrl ?>/images/system/other/candidate/mail.png" alt="">
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-md-12 pt-60 pb-20"></div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-11">
+                    <h2 class="f-30 text-black">How it works</h2>
+                    <p class="mt-30 text-light-2 text-black">Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry Lorem Ipsum has been the industry's standard Lorem Ipsum is simply
+                        dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                        standard</p>
+                </div>
+
+                <div class="col-md-12 mt-50">
+                    <div class="row circle-wrapper">
+                        <div class="col-md-10 line-wrp">
+                            <div class="line"></div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="circle">
+                                1
+                            </div>
+                            <h5 class="w-60">Register as a Employer</h5>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="circle">
+                                2
+                            </div>
+                            <h5 class="w-60">Verify Email Account</h5>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="circle">
+                                3
+                            </div>
+                            <h5 class="w-60">Create Profile</h5>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="circle">
+                                4
+                            </div>
+                            <h5 class="w-60">Purchace a package</h5>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="circle">
+                                4
+                            </div>
+                            <h5 class="w-60">Purchace a package</h5>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+<div class="container-fluid yellow-script mb-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="row">
+                    <div class="col-md-9">
+                        <h2 class="f-30 mt-5 text-black">Lorem Ipsum is simply dummy text of the</h2>
+                    </div>
+                    <div class="col-md-3">
+                        <button type="button" class="right cm-btn large text-uppercase light-blue right">Register Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
