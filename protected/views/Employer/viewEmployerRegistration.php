9e044dbedbe804b9ec6e0106f0aee3de19fcad68
Lanka <lanka@minthrm.com>
2017-09-26 22:42:47 +0530

    Employer/EmployerRegister/ page issue

--- protected/views/Employer/viewEmployerRegistration.php
+++ protected/views/Employer/viewEmployerRegistration.php
@@ -1,5 +1,7 @@
+<div class="nav-bar-space"></div>
+
 <?php $form = $this->beginWidget('CActiveForm', array('id' => 'employerRegistration')); ?>
-<section class="main-block top-space-block">
+<section class="main-block ">
     <div class="container">
         <div class="row mb-30">
 
