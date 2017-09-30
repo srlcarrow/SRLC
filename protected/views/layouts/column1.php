f58bbe8d943d087f320f8dfd7019aabdf6896e31
Lanka <lanka@minthrm.com>
2017-09-30 18:07:44 +0530

    column page

--- protected/views/layouts/column1.php
+++ protected/views/layouts/column1.php
@@ -1,6 +1,6 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div id="content">
+<div id="content" class="float-block mb-30">
 	<?php echo $content; ?>
 </div><!-- content -->
 <?php $this->endContent(); ?>
