c21b585fe47a3bcc3e188cf213c984396546ce13
Lanka <lanka@minthrm.com>
2017-10-01 00:10:27 +0530

    Employer/ProfileDetails changed

--- protected/views/Employer/ajaxLoad/package.php
+++ protected/views/Employer/ajaxLoad/package.php
@@ -1,34 +1,95 @@
 <div class="ajaxLoad_purchPackage"></div>
 
-<?php foreach ($packages as $package) { ?>
+<div class="col-md-12 pl-30 pr-0 mb-50">
+    <table class="data-table td-border-bottom">
+        <colgroup>
+            <col style="width: 15%">
+            <col style="width: 10%">
+            <col style="width: 10%">
+            <col style="width: 10%">
+            <col style="width: 10%">
+            <col style="width: 10%">
+        </colgroup>
+        <thead>
+        <tr>
+            <th>Package</th>
+            <th>Start</th>
+            <th>Expire</th>
+            <th>Used</th>
+            <th>Balance</th>
+            <th>Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td><div class="data">Package</div></td>
+            <td><div class="data">20 Jan 2017</div></td>
+            <td><div class="data">28 Jan 2017</div></td>
+            <td><div class="data">0</div></td>
+            <td><div class="data">4</div></td>
+            <td>
+                <div class="data">
+                    <span class="status-text green">Active</span>
+                </div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
+<div class="col-md-12 pl-30 pr-0">
+    <div class="col-md-12 pl-0 pr-30 package-block">
+        <div class="col-md-9">
+            <div class="col-md-7">
+                <h3 class="f-medium">Free</h3>
+                <h4 class="text-orange">0.00 LKR</h4>
+            </div>
+
+            <div class="col-md-5">
+                <h6 class="text-dark-blue mb-15">
+                    <i class="dot mr-5"></i>
+                    <span>10 Advertisement(s)</span>
+                </h6>
+
+                <h6 class="text-dark-blue">
+                    <i class="dot mr-5"></i>
+                    <span>Valid 12 Month(s)</span>
+                </h6>
+
+            </div>
+        </div>
+
+        <div class="col-md-3 mt-30 pr-0">
+            <button type="button" class="cm-btn small light-blue right" onclick="">Add Package</button>
+        </div>
+    </div>
+</div>
+
+<?php /*foreach ($packages as $package) { */?><!--
     <div class="col-md-12 pl-30 pr-0">
         <div class="col-md-12 pl-0 pr-30 package-block">
             <div class="col-md-9">
                 <div class="col-md-7">
-                    <h3 class="f-medium"><?php echo $package->pack_name; ?></h3>
-                    <h4 class="text-orange"><?php echo $package->pack_amount; ?> LKR</h4>
+                    <h3 class="f-medium"><?php /*echo $package->pack_name; */?></h3>
+                    <h4 class="text-orange"><?php /*echo $package->pack_amount; */?> LKR</h4>
                 </div>
-
                 <div class="col-md-5">
                     <h6 class="text-dark-blue mb-15">
                         <i class="dot mr-5"></i>
-                        <span><?php echo $package->pack_is_unlimited == 1 ? "Unlimited" : floor($package->pack_num_of_ads) . ' Advertisement(s)'; ?></span>
+                        <span><?php /*echo $package->pack_is_unlimited == 1 ? "Unlimited" : floor($package->pack_num_of_ads) . ' Advertisement(s)'; */?></span>
                     </h6>
-
                     <h6 class="text-dark-blue">
                         <i class="dot mr-5"></i>
-                        <span>Valid <?php echo floor($package->pack_validity_period); ?> Month(s)</span>
+                        <span>Valid <?php /*echo floor($package->pack_validity_period); */?> Month(s)</span>
                     </h6>
-
                 </div>
             </div>
-
             <div class="col-md-3 mt-30 pr-0">
-                <button type="button" class="cm-btn small light-blue right" onclick="purchasePackage(<?php echo $package->pack_id; ?>)">Add Package</button>
+                <button type="button" class="cm-btn small light-blue right" onclick="purchasePackage(<?php /*echo $package->pack_id; */?>)">Add Package</button>
             </div>
         </div>
     </div>
-<?php } ?>
+--><?php /*} */?>
 
 <script type="text/javascript">
     
